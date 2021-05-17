0ef26f5182971b5e2993dd3dd896a94901ed3418
Monty Dimkpa <cmdimkpa@gmail.com>
2021-05-17 08:35:12 +0100

    refactor and allow operator chain evaluation

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,6 +1,6 @@
 // Calculator Logic
 
-let STATE_REGISTER = {
+let stateRegister = {
     OPERAND_A : "",
     OPERAND_B : "",
     OPERATOR : null,
@@ -15,18 +15,18 @@ let STATE_REGISTER = {
  
  function C_Pressed(dummy=null){
     // clear the calculator memory
-    STATE_REGISTER["OPERAND_A"] = "";
-    STATE_REGISTER["OPERAND_B"] = "";
-    STATE_REGISTER["OPERATOR"] = null;
-    STATE_REGISTER["RESULT"] = "";
+    stateRegister.operandA = "";
+    stateRegister.operandB = "";
+    stateRegister.operator = null;
+    stateRegister.result = "";
     writeIntermediate("0");
  }
 
  function safelyEvaluate(){
      try {
-         let operand_A = +STATE_REGISTER["OPERAND_A"];
-         let operand_B = +STATE_REGISTER["OPERAND_B"];
-         let operator = STATE_REGISTER["OPERATOR"];
+         let operand_A = +stateRegister.operandA;
+         let operand_B = +stateRegister.operandB;
+         let operator = stateRegister.operator;
          let result;
          if (operator==="+") result = operand_A + operand_B;
          if (operator==="-") result = operand_A - operand_B;
@@ -42,26 +42,27 @@ let STATE_REGISTER = {
  function EQUAL_Pressed(dummy=null){
     // perform the operation:  OPERAND_A (OPERATOR) OPERAND_B
     toggle_A_over_B = true;
-    let RESULT = safelyEvaluate() || STATE_REGISTER["RESULT"]
-    STATE_REGISTER["OPERAND_A"] = "";
-    STATE_REGISTER["OPERAND_B"] = "";
-    STATE_REGISTER["RESULT"] = RESULT;
+    let RESULT = safelyEvaluate() || stateRegister.result
+    stateRegister.operandA = "";
+    stateRegister.operandB = "";
+    stateRegister.result = RESULT;
     writeIntermediate(RESULT);
  }
  
  function OPERATOR_Pressed(OPERATOR){
+   if (stateRegister.operandB !== "") EQUAL_Pressed();
    toggle_A_over_B = false;
-   STATE_REGISTER["OPERATOR"] = OPERATOR;
-   if (STATE_REGISTER["RESULT"] !== "") STATE_REGISTER["OPERAND_A"] = STATE_REGISTER["RESULT"];
+   stateRegister.operator = OPERATOR;
+   if (stateRegister.result !== "") stateRegister.operandA = stateRegister.result;
  }
  
  function DIGIT_Pressed(DIGIT){
    if (toggle_A_over_B) {
-      STATE_REGISTER["OPERAND_A"] += DIGIT;
-      writeIntermediate(STATE_REGISTER["OPERAND_A"]);
+      stateRegister.operandA += DIGIT;
+      writeIntermediate(stateRegister.operandA);
    } else {
-     STATE_REGISTER["OPERAND_B"] += DIGIT;
-     writeIntermediate(STATE_REGISTER["OPERAND_B"]);
+     stateRegister.operandB += DIGIT;
+     writeIntermediate(stateRegister.operandB);
    }
  }
 
